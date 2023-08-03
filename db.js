40f7ce0a71131a23871028af8fa510d0e9f3b251
Gurpreet Singh <gurpreetgolu31@gmail.com>
2023-08-04 04:00:37 +0530

    final change

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = process.env.DATABASE
+const mongoURI = "mongodb+srv://gurpreetgolu31:Dak2020@merntaskmanager.n3a6xlq.mongodb.net/taskmanager?retryWrites=true&w=majority"
 const connectToMongo = () => {
     mongoose.connect(mongoURI,).then(()=>    console.log("connected to mongo successfully")
     ).catch((err)=>{console.log(err)})
